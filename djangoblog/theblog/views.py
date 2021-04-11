b1c6c22085a980c313fdad42c540c9b48121481c
Xoce <ya.amerika-2015@yandex.ru>
2021-04-12 00:01:13 +0300

    '12.04.2021'

---
+++ djangoblog/theblog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+# def home(request):
+#     return render(request, 'home.html', {})
+
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_detail.html'
+
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
